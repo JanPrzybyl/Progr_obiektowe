da0deee90ba05d1b66fad352b1201d69763eb03f
Jan <jan.przybyl07@gmail.com>
2023-10-17 15:12:37 +0200

    zadanko z lekcji

--- Lekcja_17.10.2023/Program.cs
+++ Lekcja_17.10.2023/Program.cs
@@ -6,6 +6,14 @@ class Program
     {
         return a + b > c && a + c > b && b + c > a;
     }
+
+    static double PoleHeron(double a, double b, double c)
+    {
+        double p = (a + b + c) / 2;
+        double s = Math.Sqrt(p*(p - a)*(p - b)*(p - c));
+        return s;
+    }
+
     static void Main(string[] args)
     {
         // Obliczenie pola trójkąta ze wzoru herona
@@ -20,6 +28,8 @@ class Program
         //     System.Console.WriteLine("Błędne dane!");
         // }
 
+        bool CzyPoprawna = false;
+        do;
         System.Console.Write("Podaj a: ");
         double a;
         while(!double.TryParse(Console.ReadLine(), out a) || a <= 0)
@@ -43,11 +53,17 @@ class Program
 
         if(CzyTrojkat(a,b,c))
         {
-            System.Console.WriteLine("Pole trojkata o bokach {0}, {1}, {2} wynosi: ", a, b, c);
+            CzyPoprawna = true;
+            double area = PoleHeron(a,b,c);
+            Console.OutputEncoding = System.Text.Encoding.unicode;
+            System.Console.WriteLine("Pole trojkata o bokach {0}, {1}, {2} wynosi: {3:F4}cm\u00b2", a, b, c, area); // F4 - dostajemy liczbe z 4 cyframi po przecinku
+            Console.OutputEncoding = System.Text.Encoding.default;
         }
         else
         {
             System.Console.WriteLine("Podane dlugosci bokow nie tworza trojkata");
-        }
+            Thread.Sleep(2000);
+            Console.Clear();
+        }   while (!CzPoprawna);
     }
 }
