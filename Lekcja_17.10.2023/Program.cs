04c0f0bf81b4f11bd7d25f40f9c9b96d6ddcea5c
Jan <jan.przybyl07@gmail.com>
2023-10-17 19:59:24 +0200

    poprawka zadanka

--- Lekcja_17.10.2023/Program.cs
+++ Lekcja_17.10.2023/Program.cs
@@ -29,41 +29,43 @@ class Program
         // }
 
         bool CzyPoprawna = false;
-        do;
-        System.Console.Write("Podaj a: ");
-        double a;
-        while(!double.TryParse(Console.ReadLine(), out a) || a <= 0)
+        do
         {
-            System.Console.Write("Wpisz prawidłowe dane: ");
-        }
+            System.Console.Write("Podaj a: ");
+            double a;
+            while(!double.TryParse(Console.ReadLine(), out a) || a <= 0)
+            {
+                System.Console.Write("Wpisz prawidłowe dane: ");
+            }
 
-        System.Console.Write("Podaj b: ");
-        double b;
-        while(!double.TryParse(Console.ReadLine(), out b) || b <= 0)
-        {
-            System.Console.Write("Wpisz prawidłowe dane: ");
-        }
+            System.Console.Write("Podaj b: ");
+            double b;
+            while(!double.TryParse(Console.ReadLine(), out b) || b <= 0)
+            {
+                System.Console.Write("Wpisz prawidłowe dane: ");
+            }
 
-        System.Console.Write("Podaj c: ");
-        double c;
-        while(!double.TryParse(Console.ReadLine(), out c) || c <= 0)
-        {
-            System.Console.Write("Wpisz prawidłowe dane: ");
-        }
+            System.Console.Write("Podaj c: ");
+            double c;
+            while(!double.TryParse(Console.ReadLine(), out c) || c <= 0)
+            {
+                System.Console.Write("Wpisz prawidłowe dane: ");
+            }
 
-        if(CzyTrojkat(a,b,c))
-        {
-            CzyPoprawna = true;
-            double area = PoleHeron(a,b,c);
-            Console.OutputEncoding = System.Text.Encoding.unicode;
-            System.Console.WriteLine("Pole trojkata o bokach {0}, {1}, {2} wynosi: {3:F4}cm\u00b2", a, b, c, area); // F4 - dostajemy liczbe z 4 cyframi po przecinku
-            Console.OutputEncoding = System.Text.Encoding.default;
-        }
-        else
-        {
-            System.Console.WriteLine("Podane dlugosci bokow nie tworza trojkata");
-            Thread.Sleep(2000);
-            Console.Clear();
-        }   while (!CzPoprawna);
+            if(CzyTrojkat(a,b,c))
+            {
+                CzyPoprawna = true;
+                double area = PoleHeron(a,b,c);
+                // Console.OutputEncoding = System.Text.Encoding.unicode;
+                System.Console.WriteLine("Pole trojkata o bokach {0}, {1}, {2} wynosi: {3:F4}cm\u00b2", a, b, c, area); // F4 - dostajemy liczbe z 4 cyframi po przecinku
+                // Console.OutputEncoding = System.Text.Encoding.default;
+            }
+            else
+            {
+                System.Console.WriteLine("Podane dlugosci bokow nie tworza trojkata");
+                Thread.Sleep(2000);
+                Console.Clear();
+            }   
+        }while (!CzyPoprawna);
     }
 }
