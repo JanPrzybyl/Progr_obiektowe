bd0d87387ae86b50f6c8a35a2a5e1abd61c52be7
Jan <jan.przybyl07@gmail.com>
2023-10-24 15:36:21 +0200

    lekcja

---
+++ Lekcja_24.10.2023/Program.cs
@@ -0,0 +1,53 @@
+﻿namespace Lekcja_24._10._2023;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        bool flaga = false;
+        do
+        {
+            try
+            {
+                Console.WriteLine("Podaj dwie liczby\n");
+                System.Console.Write("Podaj x: ");
+                int x = int.Parse(Console.ReadLine());
+                System.Console.Write("Podaj y: ");
+                int y = int.Parse(Console.ReadLine());
+                if (y == 0)
+                {
+                    throw new Exception("You cant divide by 0");
+                }
+                double result = x / y;
+                System.Console.Write($"Wynik dzielenia {x} / {y} = ");
+                Console.ForegroundColor = ConsoleColor.Green;
+                System.Console.WriteLine($"{result:F3}");
+                Console.ResetColor();
+            }
+            catch (DivideByZeroException)
+            {
+            ErrorColorChange("You cant divide by 0");
+            }
+            catch (FormatException)
+            {
+                ErrorColorChange("\nBłędny format danych, podaj liczbe ziennoprzecinkową lub całkowitą");
+            }
+            catch (OverflowException)
+            {
+                ErrorColorChange($"Podana liczba jest błędna, podaj liczbe z zakresu <{int.MinValue} ; {int.MaxValue}>");
+            }
+            catch (Exception ex)
+            {
+                ErrorColorChange(ex.Message);
+            }
+        flaga = true;
+        }
+    }
+
+    static void ErrorColorChange(string comment)
+    {
+        Console.ForegroundColor = ConsoleColor.Red;
+        System.Console.WriteLine($"\nBłąd: {comment}");
+        Console.ResetColor();
+    }
+}
